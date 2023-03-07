8b3f0853d3ca7e6be2004c7f0ed6825e752744fa
Alma Benitez <106778890+almabntz@users.noreply.github.com>
2023-03-07 13:05:24 -0600

    added classes to sum credits and debits

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -1,7 +1,26 @@
 package com.h2;
 
 public class SavingsCalculator {
+
     private float[] credits;
     private float[] debits;
 
+//This is a constructor, it has two inputs: credits and debits.
+    //Within that constructor this.credits it telling the constructor that the inputs within it are connected to the
+    // cooresponding variables with in the SavingsCalculator class above.
+    public SavingsCalculator ( float[] credits, float[] debits) {
+        this.credits = credits;
+        this.debits = debits;
+    }
+
+    private float sumOfCredits() {
+        float sum = 0.0f;
+        for(float credit: credits) {
+            sum += credit;
+        }
+        return sum;
+    }
+private float sumOfDebits() {
+        return 0.0f;
+}
 }
